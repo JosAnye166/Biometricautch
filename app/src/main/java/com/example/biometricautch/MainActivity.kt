3e559b9f51750735aca866dead514c9951a56012
JosAnye166 <josabor33@gmail.com>
2022-05-13 17:14:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/biometricautch/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.example.biometricautch
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import com.example.biometricautch.ui.theme.BiometricautchTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BiometricautchTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Auth()
+                }
+            }
+        }
+        // Setup
+        setupAuth()
+
+    }
+    // Private methods
+
+    private var canAuthenticate = false
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+
+    private fun setupAuth() {
+        if (BiometricManager.from(this).canAuthenticate(
+                BiometricManager.Authenticators.BIOMETRIC_STRONG
+            or BiometricManager.Authenticators.DEVICE_CREDENTIAL) == BiometricManager.BIOMETRIC_SUCCESS) {
+
+            canAuthenticate = true
+
+            promptInfo = BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Autenticacion biometrica")
+                .setSubtitle("Autenticate utilizando el sensor biometrico")
+                .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG
+                        or BiometricManager.Authenticators.DEVICE_CREDENTIAL)
+                .build()
+        }
+    }
+
+    private fun authenticate(auth: (auth: Boolean) -> Unit) {
+
+        if (canAuthenticate) {
+
+            BiometricPrompt(this, ContextCompat.getMainExecutor(this),
+                object: BiometricPrompt.AuthenticationCallback() {
+
+                    override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                        super.onAuthenticationSucceeded(result)
+
+                        auth(true)
+                    }
+
+                }).authenticate(promptInfo)
+
+        } else {
+            auth(true)
+        }
+
+    }
+
+    // Composables
+    @Composable
+    fun Auth() {
+
+        var auth by remember { mutableStateOf(false) }
+
+        Column(
+            modifier = Modifier
+                .background(if (auth) androidx.compose.ui.graphics.Color.Blue else androidx.compose.ui.graphics.Color.Red )
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(if (auth) "Estas autenticado" else "Necesitas autenticacion", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+
+            Spacer(Modifier.height(8.dp))
+
+            Button(onClick = {
+                if (auth) {
+                    auth = false
+                } else {
+                    authenticate {
+                        auth = it
+                  }
+                }
+            }) {
+                Text(if (auth) "Cerrar" else "Autenticar")
+            }
+        }
+    }
+
+    @Preview(showSystemUi = true)
+    @Composable
+    fun DefaultPreview() {
+        BiometricautchTheme {
+            Auth()
+        }
+    }
+}
+
